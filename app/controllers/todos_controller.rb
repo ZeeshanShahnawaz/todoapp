dc0bb34fe24010baa17ec4692431c207e6fb8c9d
Zeeshan Shahnawaz <zeeshanshahnawaz72@gmail.com>
2018-08-27 21:46:27 +0500

    add index and links

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -28,7 +28,9 @@ class TodosController < ApplicationController
       		render 'edit'
       	  end 
       	end
-
+      	def index
+      	@todos =Todo.all
+      	end
    
    private
 def todo_params
